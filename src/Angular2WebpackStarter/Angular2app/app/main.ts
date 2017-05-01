b26f41cf4aea43d559fd232fd7690a2fae48d457
Elizabeth <elizabeth.olamisan@outlook.com>
2017-05-01 15:56:22 +0300

    Add project files.

---
+++ src/Angular2WebpackStarter/Angular2app/app/main.ts
@@ -0,0 +1,8 @@
+﻿import '../polyfills';
+import '../vendor';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from '../app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
